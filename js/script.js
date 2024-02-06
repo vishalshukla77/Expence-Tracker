cf8c60ab6332199fe4f550c9828b8e7a036b185e
vishalshukla77 <vishalshukla1131@gmail.com>
2024-02-06 15:41:21 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import ExpenseTracker from './ExpenseTracker.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const expenseTracker = new ExpenseTracker('#maincontainer');
+
+
+});
+
+
